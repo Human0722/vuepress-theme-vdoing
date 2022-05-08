ae41aea655efea4e14c7eaf0b6ab81acd1a91cbe
xueliang <btainlee@gmail.com>
2022-05-08 18:37:52 +0800

    配置GitTalk

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -269,11 +269,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: '57228208d0d1563962c7',
+          clientSecret: '305705a496d5eb29bf50bb2fe36aec6d3fceee76',
+          repo: 'blogComments', // GitHub 仓库
+          owner: 'Human0722', // GitHub仓库所有者
+          admin: ['Human0722'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
